42d0559fe56dbd0bcc425dc9b2ed61a18a801d4b
lovegupta112 <loveguptaofficials@gmail.com>
2025-10-14 15:11:43 +0530

    implemented initial setup for clickhouse

---
+++ backend/src/db/config.ts
@@ -0,0 +1,19 @@
+import { createClient } from '@clickhouse/client';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const client = createClient({
+  url: process.env.CLICKHOUSE_URL || "",
+  username: process.env.CLICKHOUSE_USERNAME || "",
+  password: process.env.CLICKHOUSE_PASSWORD || "",
+  database: process.env.CLICKHOUSE_DATABASE || "",
+})
+
+void (async () => {
+  const rows = await client.query({
+    query: 'SELECT 1',
+    format: 'JSONEachRow',
+  })
+  console.log('Result: ', await rows.json())
+})()
